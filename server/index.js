b3f55629e2c43b504e5125cbc2f9bb23db601d47
Bidhan Roy <bidhanroy578@gmail.com>
2025-03-29 13:49:37 +0600

    added services list to mongo db collection and made an api to serve the service list ,

--- server/index.js
+++ server/index.js
@@ -1,10 +1,48 @@
 require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("car doctor server runnig");
 });
+
+const uri = `mongodb+srv://${process.env.user}:${process.env.pass}@cluster0.2umnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const servicesListCollection = client
+      .db("carDoctorDB")
+      .collection("servicesList");
+    app.get("/services", async (req, res) => {
+      const options = {
+        projection: { title: 1, price: 1, img: 1 },
+      };
+      const result = await servicesListCollection.find({}, options).toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(process.env.PORT, () => {
   console.log(`car doctor server running on port ${process.env.PORT}`);
