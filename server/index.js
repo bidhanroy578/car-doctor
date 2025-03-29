89b080c6365c3b4eb9a36566990ef5dc8febfe9c
Bidhan Roy <bidhanroy578@gmail.com>
2025-03-29 15:06:54 +0600

    added cors policy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("car doctor server runnig");
