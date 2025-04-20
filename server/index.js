8f7e97f2fc3a7da286011367f7bc2617d24fb549
Bidhan Roy <bidhanroy578@gmail.com>
2025-04-21 00:05:46 +0600

    added cors policy and added jwt token as cookie on login or signup. clear cookie on logout .

--- server/index.js
+++ server/index.js
@@ -6,14 +6,15 @@ const app = express();
 const jwt = require("jsonwebtoken");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("car doctor server runnig");
-});
-
-app.post("/jwt", (req, res) => {
-  const user = req.body;
 });
 
 const uri = `mongodb+srv://${process.env.user}:${process.env.pass}@cluster0.2umnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -34,6 +35,32 @@ async function run() {
     const servicesListCollection = client
       .db("carDoctorDB")
       .collection("servicesList");
+
+    //auth related api
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.secret, { expiresIn: "1h" });
+      console.log("generated token:", token);
+      res
+        .status(200)
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "lax",
+        })
+        .send("successfully generated token");
+    });
+
+    app.post("/logout", (req, res) => {
+      console.log("logout request received");
+      res
+        .status(200)
+        .clearCookie("token", { httpOnly: true })
+        .send("cookie cleared");
+    });
+
+    //services related apis
     app.get("/services", async (req, res) => {
       const options = {
         projection: { title: 1, price: 1, img: 1, _id: 0, service_id: 1 },
